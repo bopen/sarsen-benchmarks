34edfa148b872c73ab9b9a3453a7377202ed96a5
Mattia Almansi <m.almansi@bopen.eu>
2022-04-26 14:52:41 +0200

    add some real benchmark

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,32 +1,40 @@
-# Write the benchmarking functions here.
-# See "Writing benchmarks" in the asv docs for more information.
+import os
+import tempfile
+
+import xarray as xr
+
+from sarsen import apps
 
 
 class TimeSuite:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
+    timeout = 240
+    params = ["gamma_nearest", "gamma_bilinear"]
 
-    def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
+    def __init__(self):
+        self.datadir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..", "data")
+        )
+        self.dem_urlpath = tempfile.NamedTemporaryFile(suffix=".tif").name
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
+    def setup(self, correct_radiometry):
+        dem_raster = xr.open_dataarray(
+            os.path.join(self.datadir, "South-of-Redmond-10m.tif"), engine="rasterio"
+        )
+        dem_raster_crop = dem_raster.sel(
+            x=slice(575_000, 585_000), y=slice(5_200_000, 5_190_000)
+        )
+        dem_raster_crop.rio.to_raster(self.dem_urlpath)
 
-    def time_values(self):
-        for value in self.d.values():
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            d[key]
-
-
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+    def time_terrain_correction(self, correct_radiometry):
+        with tempfile.NamedTemporaryFile(suffix=".tif") as tmp:
+            apps.terrain_correction(
+                os.path.join(
+                    self.datadir,
+                    "S1B_IW_GRDH_1SDV_20211217T141304_20211217T141329_030066_039705_9048",
+                ),
+                measurement_group="IW/VV",
+                dem_urlpath=self.dem_urlpath,
+                correct_radiometry=correct_radiometry,
+                output_urlpath=tmp.name,
+                grouping_area_factor=(3, 3),
+            )
