348d8e7419c7aa9381abc5e54535697ad8f22dd5
Mattia Almansi <m.almansi@bopen.eu>
2022-04-27 11:57:01 +0200

    add peakmem, 5 commits only, improve cache

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sarsen-benchmarks
 [![pre-commit.ci status](https://results.pre-commit.ci/badge/github/bopen/sarsen-benchmarks/main.svg)](https://results.pre-commit.ci/latest/github/bopen/sarsen-benchmarks/main)
+
+https://bopen.github.io/sarsen-benchmarks/
--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,5 +1,5 @@
 import os
-import tempfile
+from tempfile import NamedTemporaryFile
 
 import xarray as xr
 from sarsen import apps
@@ -13,27 +13,41 @@ class TimeSuite:
         self.datadir = os.path.abspath(
             os.path.join(os.path.dirname(__file__), "..", "data")
         )
-        self.dem_urlpath = tempfile.NamedTemporaryFile(suffix=".tif").name
+        self.dem_file = NamedTemporaryFile(suffix=".tif")
 
-    def setup(self, correct_radiometry):
+    def setup(self, *params):
+        # Crop dem
         dem_raster = xr.open_dataarray(
-            os.path.join(self.datadir, "South-of-Redmond-10m.tif"), engine="rasterio"
+            os.path.join(self.datadir, "South-of-Redmond-10m.tif"),
+            engine="rasterio",
+            cache=False,
         )
         dem_raster_crop = dem_raster.sel(
             x=slice(575_000, 585_000), y=slice(5_200_000, 5_190_000)
         )
-        dem_raster_crop.rio.to_raster(self.dem_urlpath)
+        dem_raster_crop.rio.to_raster(self.dem_file.name)
 
-    def time_terrain_correction(self, correct_radiometry):
-        with tempfile.NamedTemporaryFile(suffix=".tif") as tmp:
+    def teardown(self, *params):
+        self.dem_file.close()
+
+    def terrain_correction(self, *params):
+        with NamedTemporaryFile(suffix=".tif") as tmp:
             apps.terrain_correction(
                 os.path.join(
                     self.datadir,
                     "S1B_IW_GRDH_1SDV_20211217T141304_20211217T141329_030066_039705_9048",
                 ),
                 measurement_group="IW/VV",
-                dem_urlpath=self.dem_urlpath,
-                correct_radiometry=correct_radiometry,
+                dem_urlpath=self.dem_file.name,
+                correct_radiometry=params[0],
                 output_urlpath=tmp.name,
                 grouping_area_factor=(3, 3),
+                open_dem_raster_kwargs={"cache": False},
+                cache=False,
             )
+
+    def time_terrain_correction(self, *params):
+        self.terrain_correction(*params)
+
+    def peakmem_terrain_correction(self, *params):
+        self.terrain_correction(*params)
