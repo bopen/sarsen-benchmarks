bf32073cc53c70d7a3128b5bd6e8a8310846b3b7
Mattia Almansi <m.almansi@bopen.eu>
2022-05-17 11:12:11 +0200

    Update benchmarks.py

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -42,8 +42,7 @@ class Suite:
                 correct_radiometry=params[0],
                 output_urlpath=tmp.name,
                 grouping_area_factor=(3, 3),
-                open_dem_raster_kwargs={"cache": False},
-                cache=False,
+                open_dem_raster_kwargs={"chunks": 1024},
             )
 
     def time_terrain_correction(self, *params):
